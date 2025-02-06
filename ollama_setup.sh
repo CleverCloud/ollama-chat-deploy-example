9d3a46279ca4d1ed56e63773d4ca5cf7db89a20e
Peyo <pierre.lhuillery@clever-cloud.com>
2025-02-06 13:54:54 +0100

    update du lien de dl

--- ollama_setup.sh
+++ ollama_setup.sh
@@ -6,7 +6,7 @@ mkdir -p ${BIN_DIR}
 
 ollama_start() {
     echo "Downloading and launching ollama..."
-    curl -Ls https://ollama.ai/download/ollama-linux-amd64
+    curl -Ls https://ollama.com/download/ollama-linux-amd64.tgz
     tar -zxf ollama-linux-amd64.tgz -C ${BIN_DIR}/ollama
     chmod +x ${BIN_DIR}/ollama
     ollama serve &> /dev/null &
